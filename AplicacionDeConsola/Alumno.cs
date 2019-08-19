1e835e46df8f1ec7afb8f6c7b62257679108456a
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-19 12:37:04 -0700

    Agregar archivos de proyecto.

---
+++ AplicacionDeConsola/Alumno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionDeConsola
+{
+    class Alumno
+    {
+        public string Nombre { get; set; }
+
+        public string Matricula { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Telefono { get; set; }
+
+        public Alumno(string nombre, string matricula, string direccion, string telefono)
+        {
+
+            Direccion = direccion;
+            Telefono = telefono;
+        }
+        public Alumno(string nombre, string matricula)
+        {
+            Nombre = "Sin Asignar";
+            Matricula = "Sin Asignar";
+        }
+
+
+}
+}
---
+++ AplicacionDeConsola/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionDeConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno("Jose", "Matricula", "Direccion", "Telefono");
+
+           alumno1.Nombre;
+            alumno1.Matricula;
+            alumno1.Direccion;
+            alumno1.Telefono;
+            
+
+            Console.WriteLine("Nombre: " );
+            Console.WriteLine("Matricula: ");
+            Console.WriteLine("Fecha de Nacimiento: ");
+            Console.WriteLine("Direccion: ");
+            Console.WriteLine("Telefono: " );
+        }
+    }
+}
